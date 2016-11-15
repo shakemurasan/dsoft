a65774b469a83ad6c833f1f3a60736f864f53115
ym <kiyoshi.kodama@gmail.com>
2016-11-16 00:22:53 +0900

    常にassetをコンパイルするように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -84,6 +84,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = true
 end
