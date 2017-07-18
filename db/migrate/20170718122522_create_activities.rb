020f99caa242f1c93359b658f4faf4b7cc4b820f
muramurasan <kiyoshi.kodama@gmail.com>
2017-07-18 21:40:21 +0900

    Activityモデルの追加

---
+++ db/migrate/20170718122522_create_activities.rb
@@ -0,0 +1,16 @@
+class CreateActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :activities do |t|
+      t.integer :times, null: true
+      t.string :alias_title, null: true
+      t.text :contents, null: false
+      t.string :image_path, null: true
+
+      t.timestamps
+
+      t.index :times, unique: true
+      t.index :alias_title, unique: true
+      t.index :image_path, unique: true
+    end
+  end
+end
