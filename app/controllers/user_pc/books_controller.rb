c9cd8ed1c1d3145d4b62243ce26395f78771f981
muramurasan <kiyoshi.kodama@gmail.com>
2017-07-24 00:30:17 +0900

    今までやった本の一覧を表示できるようにする

--- app/controllers/user_pc/books_controller.rb
+++ app/controllers/user_pc/books_controller.rb
@@ -1,20 +1,7 @@
 class UserPc::BooksController < ApplicationController
-  before_action :set_book, only: [:show]
-
   # GET /books
   # GET /books.json
   def index
     @books = Book.all
   end
-
-  # GET /books/1
-  # GET /books/1.json
-  def show
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 end
