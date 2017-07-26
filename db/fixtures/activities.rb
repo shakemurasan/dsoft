ae0b6d02aed425d8dadda047eefe2c02e6ff800a
muramurasan <kiyoshi.kodama@gmail.com>
2017-07-26 22:26:32 +0900

    Activityのseedを作成

---
+++ db/fixtures/activities.rb
@@ -0,0 +1,31 @@
+Activity.seed(:id,
+  {
+    id: 108,
+    times: 108,
+    alias_title: nil,
+    contents: '本勉強会で秋に開催予定のハッカソンのテーマややり方について話しました。',
+    image_path: nil,
+    order_at: '2017-07-01'
+  },
+  {
+    id: 109,
+    times: 109,
+    alias_title: nil,
+    contents: '
+    今回は珍しく、朝から晩までもくもく会でした。
+    面白い発見や疑問があったら共有しながら、それぞれ自分の好きなことをやりました。',
+    image_path: nil,
+    order_at: '2017-07-02'
+  },
+  {
+    id: 110,
+    times: 110,
+    alias_title: nil,
+    contents: '
+    サイバーセキュリティプログラミングの3章を読みました。
+    rawソケットを流れる全てのパケットを監視するという内容でした。
+    Pythonで構造体の入出力が簡単できることが驚きでした。',
+    image_path: nil,
+    order_at: '2017-07-03'
+  },
+)
