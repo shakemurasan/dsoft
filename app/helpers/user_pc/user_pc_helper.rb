a0994d33a3a5d0c40fbd1d856fab2cd54ed86f73
muramurasan <kiyoshi.kodama@gmail.com>
2017-07-27 16:44:49 +0900

    helperのimageが動作していなかったので修正

--- app/helpers/user_pc/user_pc_helper.rb
+++ app/helpers/user_pc/user_pc_helper.rb
@@ -2,7 +2,7 @@ module UserPc
   # TODO: 名前空間とHelper名が重複しているので、適当な命名を考える
   module UserPcHelper
     def image(property, model)
-      '<img class="#{property[:class]}" src="#{model.image_path}">'.html_safe if model.image_path.present?
+      image_tag(model.image_path, class: property[:class]) if model.image_path.present?
     end
   end
 end
