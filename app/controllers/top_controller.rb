fd7f6902e005b25af0d9a90e40fe8cf234599f7d
muramurasan <kiyoshi.kodama@gmail.com>
2017-07-27 21:19:22 +0900

    pureレイアウトを調整しつつ、Activityを動的に表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
     @books = Book.where(current: true).order(order_at: :desc)
-    @activities = Activity.limit(3).order(order_at: :desc)
+    @activities = Activity.limit(2).order(order_at: :desc)
   end
 end
