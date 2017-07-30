4c7567fc1dcadfadf4f088f2792ab75d4641fcd1
che <che@iBook500.local>
2017-07-30 12:14:22 +0900

    fix year

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@ class TopController < ApplicationController
   def index
     @books = Book.where(current: true).order(order_at: :desc)
     @activities = Activity.limit(2).order(order_at: :desc)
-    @grade = Date.today.year - 2012
+    @grade = Date.today.year - 2011
   end
 end
