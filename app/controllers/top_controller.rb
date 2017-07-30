06aebc38a00d1c69d58c04b2bbc67c890571834b
che <che@iBook500.local>
2017-07-30 12:12:11 +0900

    update a grade number dynamically

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,6 @@ class TopController < ApplicationController
   def index
     @books = Book.where(current: true).order(order_at: :desc)
     @activities = Activity.limit(2).order(order_at: :desc)
+    @grade = Date.today.year - 2012
   end
 end
